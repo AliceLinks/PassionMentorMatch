30fd2ef9b02d5e3661f1a694c511fb3c17d72328
AliceLinks <231250091@smail.nju.edu.cn>
2025-12-28 17:38:48 +0800

    update：美化 tabbar

--- frontend/custom-tab-bar/index.js
+++ frontend/custom-tab-bar/index.js
@@ -3,9 +3,9 @@ Component({
     visible: true,
     selected: 0,
     list: [
-      { pagePath: "/pages/home/index", text: "主页", icon: "🏠" },
-      { pagePath: "/pages/course/index", text: "约课", icon: "📅" },
-      { pagePath: "/pages/profile/index", text: "我的", icon: "👤" }
+      { pagePath: "/pages/home/index", text: "主页", icon: "/custom-tab-bar/icons/home.png", selectedIcon: "/custom-tab-bar/icons/home-active.png" },
+      { pagePath: "/pages/course/index", text: "约课", icon: "/custom-tab-bar/icons/calendar.png", selectedIcon: "/custom-tab-bar/icons/calendar-active.png" },
+      { pagePath: "/pages/profile/index", text: "我的", icon: "/custom-tab-bar/icons/user.png", selectedIcon: "/custom-tab-bar/icons/user-active.png" }
     ]
   },
   attached() {
